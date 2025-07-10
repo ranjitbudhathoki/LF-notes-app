4fd7815bff403db53074832829891d39180a4b58
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-07-10 21:55:13 +0545

    feat: add global error handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,38 @@ import authRouter from "./api/auth.js";
 import verifyAuth from "./utils/verifyAuth.js";
 import notesRouter from "./api/notes.js";
 import categoriesRouter from "./api/categories.js";
+import { HTTPException } from "hono/http-exception";
 const app = new Hono();
 
-app.get("/", verifyAuth, async (c) => {
-  return c.text("Hello Hono!");
+app.onError((err, c) => {
+  if (err.name === "TokenExpiredError") {
+    return c.json(
+      {
+        message: "Token has expired. Please login again",
+      },
+      401,
+    );
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    return c.json(
+      {
+        message: "Invalid token. Please login again",
+      },
+      401,
+    );
+  }
+
+  return c.json(
+    {
+      message: "Internal server error",
+      error:
+        process.env.NODE_ENV === "development"
+          ? err.message
+          : "Something went wrong",
+    },
+    500,
+  );
 });
 
 app.route("/api/auth", authRouter);
