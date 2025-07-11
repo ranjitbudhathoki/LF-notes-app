b0544283fa544c4aec56a37fd243b8ebdf800f1c
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-07-11 09:45:15 +0545

    feat: integrate signup

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -3,9 +3,18 @@ interface LoginCredentials {
   email: string;
   password: string;
 }
-
+interface SignUpCredentials {
+  name: string;
+  email: string;
+  password: string;
+}
 export async function loginApi({ email, password }: LoginCredentials) {
   const { data } = await _axios.post("/auth/login", { email, password });
+  return data;
+}
+
+export async function signUpApi({ name, email, password }: SignUpCredentials) {
+  const { data } = await _axios.post("/auth/signup", { name, email, password });
   return data;
 }
 
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,9 +1,53 @@
+import { signUpApi } from "@/api/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router";
+import useAuth from "@/hooks/useAuth";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import toast from "react-hot-toast";
+import { Link, Navigate, useLocation, useNavigate } from "react-router";
+
+interface Inputs {
+  email: string;
+  password: string;
+  name: string;
+}
 
 export default function SignupPage() {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const { setIsAuthenticated, isAuthenticated, setUser } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (data: Inputs) => signUpApi(data),
+    onSuccess: (data) => {
+      setIsAuthenticated(true);
+      setUser(data.result);
+      toast.success("Signup successful!");
+      navigate("/notes");
+    },
+    onError: (error: unknown) => {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data?.message || "Something went wrong");
+      } else {
+        toast.error("Unexpected error occurred");
+      }
+    },
+  });
+
+  if (isAuthenticated) {
+    const fromPath = location.state?.path || "/notes";
+    return <Navigate to={fromPath} replace />;
+  }
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    mutate(data);
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center px-4">
       <div className="w-full max-w-sm">
@@ -13,7 +57,7 @@ export default function SignupPage() {
         </div>
 
         <div className="bg-white rounded-2xl p-8 shadow-sm border border-gray-100">
-          <form className="space-y-5">
+          <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
             <div>
               <Label
                 htmlFor="name"
@@ -23,11 +67,11 @@ export default function SignupPage() {
               </Label>
               <Input
                 id="name"
-                name="name"
                 type="text"
                 placeholder="John Doe"
                 className="mt-1"
                 required
+                {...register("name")}
               />
             </div>
 
@@ -40,11 +84,11 @@ export default function SignupPage() {
               </Label>
               <Input
                 id="email"
-                name="email"
                 type="email"
                 placeholder="you@example.com"
                 className="mt-1"
                 required
+                {...register("email")}
               />
             </div>
 
@@ -57,17 +101,17 @@ export default function SignupPage() {
               </Label>
               <Input
                 id="password"
-                name="password"
                 type="password"
                 placeholder="••••••••"
                 className="mt-1"
                 required
                 minLength={6}
+                {...register("password")}
               />
             </div>
 
-            <Button type="submit" className="w-full">
-              Create Account
+            <Button type="submit" className="w-full" disabled={isPending}>
+              {isPending ? "Creating Account..." : "Create Account"}
             </Button>
           </form>
 
