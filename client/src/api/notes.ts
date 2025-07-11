6e96633c9761f2ae267821a2cb1fe18f0a88c77d
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-07-11 23:25:59 +0545

    feat: create a form to add notes along with ReactQuill editor

--- client/src/api/notes.ts
+++ client/src/api/notes.ts
@@ -4,3 +4,12 @@ export async function getNotesApi() {
   const { data } = await _axios.get("/notes");
   return data;
 }
+interface CreateNotePayload {
+  title: string;
+  content: string;
+  categoryIds: number[];
+}
+export async function createNoteApi(payload: CreateNotePayload) {
+  const { data } = await _axios.post("/notes", payload);
+  return data;
+}
