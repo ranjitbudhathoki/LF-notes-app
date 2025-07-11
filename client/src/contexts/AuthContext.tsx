f1c195192e635e1c1c250f121e91d787c9878443
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-07-11 07:59:26 +0545

    feat: create auth context

---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,62 @@
+import _axios from "@/config/axios";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUserApi } from "@/api/auth";
+
+interface User {
+  id: number;
+  email: string;
+  username: string;
+}
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  user: User | null;
+  setUser: (user: User | null) => void;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["currentUser"],
+    queryFn: getCurrentUserApi,
+  });
+
+  useEffect(() => {
+    console.log("useEFfecte called");
+    console.log("data", data);
+    if (data) {
+      setUser(data);
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ user, setIsAuthenticated, isAuthenticated, setUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
