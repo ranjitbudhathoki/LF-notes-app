5258c11be7c671780cb07c70bcf71f5c2b4cc3d8
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-07-13 22:13:48 +0545

    fix: add validation message for categories

--- server/src/utils/validators/categories.ts
+++ server/src/utils/validators/categories.ts
@@ -1,9 +1,16 @@
 import { z } from "zod/v4";
 
 export const createCategorSchema = z.object({
-  name: z.string().trim().min(3),
+  name: z
+    .string({ message: "Category name is required" })
+    .trim()
+    .min(3, { message: "Category name must be at least 3 characters long" }),
 });
 
 export const updateCategorySchema = z.object({
-  name: z.string().trim().min(3).optional(),
+  name: z
+    .string()
+    .trim()
+    .min(3, { message: "Category name must be at least 3 characters long" })
+    .optional(),
 });
