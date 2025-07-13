ce8d96797131b5ab6a744c995dbaa46f3bda185a
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-07-13 22:13:04 +0545

    fix: add validation message for auth

--- server/src/utils/validators/auth.ts
+++ server/src/utils/validators/auth.ts
@@ -2,17 +2,20 @@ import { z } from "zod/v4";
 
 export const registerSchema = z.object({
   name: z
-    .string()
+    .string({ message: "Name is required" })
     .trim()
     .min(2, { message: "Name must be at least 2 characters long" }),
-  email: z.email({ message: "Invalid email " }).trim(),
+
+  email: z.email({ message: "Invalid email address" }).trim(),
+
   password: z
-    .string()
+    .string({ message: "Password is required" })
     .trim()
     .min(8, { message: "Password must be at least 8 characters long" }),
 });
 
 export const loginSchema = z.object({
-  email: z.email().trim(),
-  password: z.string().trim(),
+  email: z.email({ message: "Invalid email address" }).trim(),
+
+  password: z.string({ message: "Password is required" }).trim(),
 });
